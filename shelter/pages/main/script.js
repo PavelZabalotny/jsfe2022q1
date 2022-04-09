bc52a0a2a7c6c070212d4e9e5e3978139b11cb9e
PavelZabalotny <pavelzabalotny@gmail.com>
2022-04-09 21:18:18 +0300

    feat: implement burger menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,12 @@
-console.log('Shelter main')
+import Burger from "./burger.js"
+
+const domElements = {
+	burger: document.querySelector('.burger'),
+	header: document.querySelector('.header'),
+	headerShadow: document.querySelector('.header__shadow'),
+	logo: document.querySelector('#logo'),
+	headerLink: document.querySelectorAll('.menu__link'),
+}
+
+const burger = new Burger(domElements)
+burger.init()
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,1 +1,14 @@
-/**/
+import Burger from "../main/burger.js"
+
+const domElements = {
+	burger: document.querySelector('.burger'),
+	header: document.querySelector('.header'),
+	headerShadow: document.querySelector('.header__shadow'),
+	logo: document.querySelector('#logo'),
+	headerLink: document.querySelectorAll('.menu__link'),
+	burgerHeader: document.querySelector('.burger-header'),
+	pets: document.querySelector('.pets'),
+}
+
+const burger = new Burger(domElements)
+burger.init()
